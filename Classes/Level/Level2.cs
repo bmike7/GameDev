bfd07cf45af7de484e3102709afc6a0461a85cc3
bmike7 <bijl.mike@outlook.be>
2020-08-03 11:48:42 +0200

    made the second level a bit more interesting;

--- Classes/Level/Level2.cs
+++ Classes/Level/Level2.cs
@@ -13,26 +13,40 @@ namespace RogueSimulator.Classes.Level
             NONE,
             GROUND,
             LEFTWALL,
+            LEFTWALL_GROUND_LEVEL,
+            LEFT_TOP_CORNER,
             RIGHTWALL,
-            BLOCK
+            RIGHTWALL_GROUND_LEVEL,
+            RIGHT_TOP_CORNER,
+            BLOCK,
+            LEFTSIDE_FLOATING_BLOCK,
+            RIGHTSIDE_FLOATING_BLOCK,
         }
 
         private Dictionary<TileType, Rectangle> _tileTypes = new Dictionary<TileType, Rectangle> {
             {TileType.GROUND, new Rectangle(90, 30, 30, 30)},
             {TileType.LEFTWALL, new Rectangle(64, 56, 30, 30)},
+            {TileType.LEFTWALL_GROUND_LEVEL, new Rectangle(257, 273, 30, 30)},
+            {TileType.LEFT_TOP_CORNER, new Rectangle(64, 32, 30, 30)},
             {TileType.RIGHTWALL, new Rectangle(115, 56, 30, 30)},
+            {TileType.RIGHTWALL_GROUND_LEVEL, new Rectangle(193, 273, 30, 30)},
+            {TileType.RIGHT_TOP_CORNER, new Rectangle(114, 32, 30, 30)},
             {TileType.BLOCK, new Rectangle(160, 32, 30, 30)},
+            {TileType.LEFTSIDE_FLOATING_BLOCK, new Rectangle(128, 128, 30, 30)},
+            {TileType.RIGHTSIDE_FLOATING_BLOCK, new Rectangle(192, 128, 30, 30)}
         };
 
-        private const int NUMBER_OF_LINES = 3;
-        private const int NUMBER_OF_COLUMNS = 30;
+        private const int NUMBER_OF_LINES = 5;
+        private const int NUMBER_OF_COLUMNS = 50;
         private const int NEAR_DISTANCE = 200;
         private const int BACKGROUND_PIXEL_WIDTH = 640;
         private int[,] _levelDesign = new int[NUMBER_OF_LINES, NUMBER_OF_COLUMNS]
         {
-            {0,0,0,0,0,0,0,4,0,4,0,0,0,0,0,0,0,0,0,4,0,0,2,3,0,0,0,0,0,0},
-            {0,0,0,0,0,2,3,0,0,4,0,0,0,0,0,0,0,0,0,4,0,0,2,3,0,0,0,0,0,0},
-            {1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,9,10,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,9,10,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,8,0,8,0,0,0,0,0,0,0,0,0,0,0,0,4,7,0,0,0,0,0,0,0,8,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,4,7,0,0,8,0,0,0,0,0,0,0,0,0,8,0,0,2,5,0,0,0,0,0,9,10,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {1,1,1,1,1,3,6,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,3,6,1,1,0,0,0,0,0,0,0,0,8,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1},
         };
         private List<Tile> _tiles = new List<Tile>();
 
