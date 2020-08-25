1a3b0457fbfc895d56e0d89b9903c48c37323321
bmike7 <bijl.mike@outlook.be>
2020-08-25 19:46:01 +0200

    added a second type of enemy to the second level;

---
+++ Classes/Entity/Skeleton.cs
@@ -0,0 +1,28 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+using RogueSimulator.Classes.Mechanics;
+
+namespace RogueSimulator.Classes.Entity
+{
+    public class Skeleton : Character
+    {
+        public const string ASSET_NAME = "SpriteSheets/Enemies/Skeleton/AllSkelletonActions";
+        public Skeleton(Texture2D texture, Vector2 position)
+            : base(
+                input: new AI(),
+                texture: texture,
+                position: position,
+                collisionRectangle: new Rectangle((int)position.X, (int)position.Y, 50, 65),
+                health: 150
+            )
+        {
+            _scale = 1.6f;
+            _movement.HorizontalVelocity = 100;
+            _actionAnimations.Add(MovementAction.IDLE, new Animation(65, 365, 45, 51, 151, 4));
+            _actionAnimations.Add(MovementAction.RUN, new Animation(65, 830, 46, 52, 151, 4));
+            _actionAnimations.Add(MovementAction.HIT, new Animation(65, 675, 45, 51, 151, 4, true));
+            _actionAnimations.Add(MovementAction.DIE, new Animation(65, 210, 46, 53, 151, 4, true));
+        }
+    }
+}
